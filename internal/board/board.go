14b447cbb78f19fe14be1026b74a19ca5fbd8083
Anthony Buchholz <anthony.buchholz@gmail.com>
2023-09-21 10:11:35 -0500

    Added a few comments to fix

--- internal/board/board.go
+++ internal/board/board.go
@@ -7,24 +7,26 @@ import (
 )
 
 type Board struct {
-    Blocks [2][2]block.Block
+	Blocks [2][2]block.Block
 }
 
+// TODO: have t be type Marble rather than type int
 func (b Board) PlaceMarble(c, d, x, y, t int) Board {
-    // FIXME: c,d are the coords of the block, and x,y are coords within the block, t is type of marble (b/w). Dunno if there's better convention for these var names.
-    // TODO: Make it so a player can't try to put a marble where another marble has already been placed.
-    marbleTypeUsed := marble.EmptyMarble
-    if t == 0 {
-        marbleTypeUsed = marble.BlackMarble
-    } else if t == 1 {
-        marbleTypeUsed = marble.WhiteMarble
-    } else {
-        fmt.Println("Error! internal/board/board.go func PlaceMarble got passed in an invalid value for t (b/w marble!)")
-        // FIXME: Why won't this error message work instead of the simple Println?
-        // t.Errorf("Error! internal/board/board.go func PlaceMarble got passed in an invalid value for t (b/w marble!)")
-}
-blockUsed := b.Blocks[c][d]
-blockUsed.Places[x][y] = marbleTypeUsed
+	// FIXME: c,d are the coords of the block, and x,y are coords within the block, t is type of marble (b/w). Dunno if there's better convention for these var names.
+	// TODO: Make it so a player can't try to put a marble where another marble has already been placed.
+	marbleTypeUsed := marble.EmptyMarble
+	if t == 0 {
+		marbleTypeUsed = marble.BlackMarble
+	} else if t == 1 {
+		marbleTypeUsed = marble.WhiteMarble
+	} else {
+		fmt.Println("Error! internal/board/board.go func PlaceMarble got passed in an invalid value for t (b/w marble!)")
+		// FIXME: Why won't this error message work instead of the simple Println?
+		// t.Errorf("Error! internal/board/board.go func PlaceMarble got passed in an invalid value for t (b/w marble!)")
+	}
+	blockUsed := b.Blocks[c][d]
+	blockUsed.Places[x][y] = marbleTypeUsed
 
-    return b
+	return b
 }
+
--- internal/board/board_test.go
+++ internal/board/board_test.go
@@ -1,23 +1,28 @@
 package board
 
 import (
-	"testing"
 	"internal/block"
 	"internal/marble"
+	"testing"
 )
 
 func TestPlaceMarble(t *testing.T) {
-    var s [2][2]block.Block
+	var s [2][2]block.Block
 	b := Board{s}
 	b.PlaceMarble(0, 1, 2, 0, 0) // *Should* put marble in the top right corner of the bottom left block.
+
+	// Added this check temporarily since your test wasn't correctly working
+	if b.Blocks[0][1].Places[2][0] != marble.BlackMarble {
+		t.Errorf("Black marble not found")
+	}
 
 	for i := 0; i < 2; i++ {
 		for j := 0; j < 2; j++ {
 			for k := 0; k < 3; k++ {
 				for l := 0; l < 3; l++ {
-					// placement := b.Blocks[i][j].Places[k][l] // FIXME: Is it easier to use shorthand like this, or 
+					// placement := b.Blocks[i][j].Places[k][l] // FIXME: Is it easier to use shorthand like this, or
 					if i == 0 && j == 1 {
-						if k == 2 && j == 0 && b.Blocks[i][j].Places[k][l] != marble.BlackMarble {
+						if k == 2 && j == 0 && b.Blocks[i][j].Places[k][l] != marble.BlackMarble { // FIXME  This if statement should never be hit because the previous if statement checks to make sure that j=1 and this checks for j=0 so one will be false making this if statement impossible to enter
 							t.Errorf("Black marble not found at %d, %d", k, l)
 						} else if b.Blocks[i][j].Places[k][l] != marble.EmptyMarble {
 							t.Errorf("Empty marble not found at %d, %d", k, l)
@@ -27,7 +32,7 @@ func TestPlaceMarble(t *testing.T) {
 							t.Errorf("Empty marble not found at %d, %d", k, l)
 						}
 					}
-				} 
+				}
 			}
 		}
 	}
